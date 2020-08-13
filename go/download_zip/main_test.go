6f26db30f2da1e9145fc5524b2b518fd7d3815d8
ycc <41176493+hyperxcode@users.noreply.github.com>
2020-08-13 09:07:18 +0800

    Update main_test.go

--- go/download_zip/main_test.go
+++ go/download_zip/main_test.go
@@ -40,9 +40,11 @@ func TestHttp( t *testing.T ){
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		t.Log("test http err:"+ err.Error())
-	}else{
+	}else if(body != nil){
 		CopyFile(body, "download.zip")
 		t.Log("test http copy file")
+	} else{
+		t.Log("test http body empty")
 	}
 }
 
